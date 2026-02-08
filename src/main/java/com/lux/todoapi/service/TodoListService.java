e012e19fb0a825348d0fe3f0f3c4c784acc1590a
PiyushDaulatkar <piyushdaulatkar03@gmail.com>
2026-02-08 15:48:30 +0530

    Mutation query to add todoItem to todoList.

--- src/main/java/com/lux/todoapi/service/TodoListService.java
+++ src/main/java/com/lux/todoapi/service/TodoListService.java
@@ -1,7 +1,9 @@
 package com.lux.todoapi.service;
 
+import com.lux.todoapi.entity.TodoItem;
 import com.lux.todoapi.entity.TodoList;
 import com.lux.todoapi.entity.User;
+import com.lux.todoapi.repository.TodoItemRepository;
 import com.lux.todoapi.repository.TodoListRepository;
 import com.lux.todoapi.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -13,11 +15,14 @@ public class TodoListService {
 
     private final TodoListRepository todoListRepository;
     private final UserRepository userRepository;
+    private final TodoItemRepository todoItemRepository;
 
     public TodoListService(TodoListRepository todoListRepository,
-            UserRepository userRepository) {
+            UserRepository userRepository,
+            TodoItemRepository todoItemRepository) {
         this.todoListRepository = todoListRepository;
         this.userRepository = userRepository;
+        this.todoItemRepository = todoItemRepository;
     }
 
     public List<TodoList> getAllTodoLists() {
@@ -31,5 +36,13 @@ public class TodoListService {
         todoList.setPublic(isPublic);
 
         return todoListRepository.save(todoList);
+    }
+
+    public TodoItem addTodoItem(Long todoListId, String text) {
+        TodoList todoList = todoListRepository.findById(todoListId)
+                .orElseThrow(() -> new RuntimeException("TodoList not found"));
+
+        TodoItem todoItem = new TodoItem(text, todoList);
+        return todoItemRepository.save(todoItem);
     }
 }
